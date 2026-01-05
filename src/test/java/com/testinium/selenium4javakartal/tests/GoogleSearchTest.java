4325f305bb2764196489768b709ae30ced1ce14b
Furkan Kartal <Furkan.kartal@testinium.com>
2026-01-05 08:48:07 +0300

    first commit

--- src/test/java/com/testinium/selenium4javakartal/tests/GoogleSearchTest.java
+++ src/test/java/com/testinium/selenium4javakartal/tests/GoogleSearchTest.java
@@ -10,29 +10,6 @@ import static org.junit.jupiter.api.Assertions.*;
 public class GoogleSearchTest extends BaseTest {
 
     @Test
-    public void searchSelenium() {
-        driver.get("https://www.google.com");
-
-        // Dismiss cookie consent if present
-        try {
-            WebElement consentButton = driver.findElement(
-                    By.xpath("//div[contains(@class,'VfPpkd-RLmnJb') or @id='L2AGLb']")
-            );
-            if (consentButton.isDisplayed()) {
-                consentButton.click();
-            }
-        } catch (Exception ignored) {}
-
-        WebElement searchBox = driver.findElement(By.name("q"));
-        searchBox.sendKeys("Selenium");
-        searchBox.submit();
-
-        WebElement results = driver.findElement(By.id("search"));
-        assertTrue(results.isDisplayed());
-        assertTrue(driver.getTitle().toLowerCase().contains("selenium"));
-    }
-
-    @Test
     public void openAmazonAndPrintTitle() {
         driver.get("https://www.amazon.com");
         System.out.println("Page title: " + driver.getTitle());
